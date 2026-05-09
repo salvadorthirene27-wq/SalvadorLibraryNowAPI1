4b1cbe96d0b40ceb9bd7fb0331bef4a9827182b6
STUDENT 13 <STUDENT 13@DESKTOP-V9ERIQ0>
2026-05-09 17:47:45 +0800

    Add project files.

---
+++ Controlers/BooksController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SalvadorLibraryNowAPI.Models;
+using System.Net.NetworkInformation;
+
+namespace SalvadorLibraryNowAPI.Controlers
+{
+    [Route("api/v1/books")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>
+
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "As Old As Time: A Twisted Tale",
+                Author = "Elizabeth J. Braswell",
+                Genre = "Young Adult / Fantacy / Fairy Tale Retelling",
+                Available = true,
+                Publishedyear = 2016
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "What Once Was Mine: A Twisted Tale",
+                Author = "Elizabeth J. Braswell",
+                Genre = "Young Adult / Fantacy / Fairy Tale Retelling",
+                Available = true,
+                Publishedyear = 2021
+            }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new
+            {
+                status = "sucess",
+                data = books,
+                message = "Books retrived."
+            });
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not found."
+                });
+            return Ok(new
+            {
+                status = "success",
+                data = book,
+                message = "Book retrived."
+            });
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] Book newBook)
+        {
+            newBook.Id = books.Count + 1;
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetById),
+                new { id = newBook.Id },
+                new
+                {
+                    status = "success",
+                    data = newBook,
+                    message = "Book created."
+                });
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Book updateBook)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not found."
+                });
+            book.Title = updateBook.Title;
+            book.Author = updateBook.Author;
+            book.Genre = updateBook.Genre;
+            book.Available = updateBook.Available;
+            book.Publishedyear = updateBook.Publishedyear;
+
+            return Ok(new
+            {
+                status = "success",
+                data = book,
+                message = "Book updated."
+            });
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not found."
+                });
+
+            books.Remove(book);
+            return Ok(new
+            {
+                status = "success",
+                data = (object?)null,
+                message = "Book deleted."
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://+:{port}");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
